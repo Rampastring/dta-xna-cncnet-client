c2d9bebb025ddb5d88b016d2b339b21c800254a6
Rampastring <rampastring@outlook.com>
2022-01-25 00:35:29 +0200

    Make CnCNetLobby parse spawnSG.ini from the correct path

--- DXMainClient/DXGUI/Multiplayer/CnCNet/CnCNetLobby.cs
+++ DXMainClient/DXGUI/Multiplayer/CnCNet/CnCNetLobby.cs
@@ -731,7 +731,7 @@ namespace DTAClient.DXGUI.Multiplayer.CnCNet
                 }
                 else
                 {
-                    IniFile spawnSGIni = new IniFile(ProgramConstants.GamePath + "Saved Games/spawnSG.ini");
+                    IniFile spawnSGIni = new IniFile(ProgramConstants.GamePath + ProgramConstants.SAVED_GAME_SPAWN_INI);
                     password = Utilities.CalculateSHA1ForString(
                         spawnSGIni.GetStringValue("Settings", "GameID", string.Empty)).Substring(0, 10);
                 }
